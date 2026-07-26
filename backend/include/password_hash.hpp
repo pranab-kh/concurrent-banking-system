179f38c29bec09bca3038b8deca89359c8fcbb37
SakarDallakoti <081bct069.sakar@pcampus.edu.np>
2026-07-26 13:09:07 +0545

    added pasword hashing and a lot of libraries for it

---
+++ backend/include/password_hash.hpp
@@ -0,0 +1,43 @@
+#ifndef PASSWORD_HASH_HPP
+#define PASSWORD_HASH_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include "BCrypt.hpp"
+
+bool hash_password(std::string &password)
+{
+    const char *pepper = std::getenv("PEPPER");
+
+    if (!pepper)
+    {
+        std::cerr << "Environment Variable Not Set" << std::endl;
+        return false;
+    }
+
+    password = password + std::string(pepper);
+    
+    password = BCrypt::generateHash(password, 12);
+    return true;
+
+   
+
+    return true;
+}
+bool verify_password(std::string &password, std::string &hashed_password)
+{
+    const char *pepper = std::getenv("PEPPER");
+
+    if (!pepper)
+    {
+        std::cerr << "Environment Variable Not Set" << std::endl;
+        return false;
+    }
+
+    password = password + std::string(pepper);
+
+     bool is_match = BCrypt::validatePassword(password, hashed_password);
+    return is_match;
+}
+
+#endif
